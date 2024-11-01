f070bc934d89a65eed4615dbdfc940d9ddd3d499
aliraza-zaidi <alirazaidi.official@gmail.com>
2024-11-01 23:42:03 +0500

    Handled Background Processes

--- v3.c
+++ v3.c
@@ -40,9 +40,17 @@ void handle_sigchld(int sig) {
     while (waitpid(-1, NULL, WNOHANG) > 0);
 }
 
-int process_command(char* cmdline){
+int process_command(char* cmdline) {
     char* cmds[MAXARGS];
     int cmd_count = 0;
+    int is_background = 0;
+
+    // Check for background process indicator '&'
+    int len = strlen(cmdline);
+    if (cmdline[len - 1] == '&') {
+        is_background = 1;
+        cmdline[len - 1] = '\0'; // Remove '&' from the command
+    }
 
     // Split the command line by the pipe symbol "|"
     char* token = strtok(cmdline, "|");
@@ -50,13 +58,14 @@ int process_command(char* cmdline){
         cmds[cmd_count++] = token;
         token = strtok(NULL, "|");
     }
+
     int in_fd = 0; // Initial input is stdin
     int fd[2];
 
     // Loop through each command
     for (int i = 0; i < cmd_count; i++) {
         char** arglist = tokenize(cmds[i]);
-        
+
         // Variables for input and output redirection
         int input_redirect = -1;
         int output_redirect = -1;
@@ -64,23 +73,21 @@ int process_command(char* cmdline){
         // Check for input/output redirection in arguments
         for (int j = 0; arglist[j] != NULL; j++) {
             if (strcmp(arglist[j], "<") == 0) {
-                // Input redirection
                 input_redirect = open(arglist[j + 1], O_RDONLY);
                 if (input_redirect < 0) {
                     perror("Input file open failed");
                     return -1;
                 }
                 free(arglist[j]);
-                arglist[j] = NULL; // Remove "<" from arglist
+                arglist[j] = NULL;
             } else if (strcmp(arglist[j], ">") == 0) {
-                // Output redirection
                 output_redirect = open(arglist[j + 1], O_WRONLY | O_CREAT | O_TRUNC, 0644);
                 if (output_redirect < 0) {
                     perror("Output file open failed");
                     return -1;
                 }
                 free(arglist[j]);
-                arglist[j] = NULL; // Remove ">" from arglist
+                arglist[j] = NULL;
             }
         }
 
@@ -89,40 +96,44 @@ int process_command(char* cmdline){
             pipe(fd);
         }
 
-        if (fork() == 0) {
+        pid_t pid = fork();
+        if (pid == 0) {
             // Child process
 
             // Redirect input
             if (input_redirect != -1) {
-                dup2(input_redirect, 0); // Redirect stdin to input file
+                dup2(input_redirect, 0);
                 close(input_redirect);
             } else if (in_fd != 0) {
-                dup2(in_fd, 0); // Set stdin to the last pipe’s read end
+                dup2(in_fd, 0);
                 close(in_fd);
             }
 
             // Redirect output
             if (output_redirect != -1) {
-                dup2(output_redirect, 1); // Redirect stdout to output file
+                dup2(output_redirect, 1);
                 close(output_redirect);
             } else if (i < cmd_count - 1) {
-                dup2(fd[1], 1); // Set stdout to the pipe’s write end
+                dup2(fd[1], 1);
                 close(fd[1]);
             }
 
-            // Execute command
             execvp(arglist[0], arglist);
             perror("exec failed");
             exit(1);
         }
 
-        // Parent process closes used pipe ends and updates in_fd for next command
+        if (!is_background) {
+            // Wait for the foreground process to finish
+            waitpid(pid, NULL, 0);
+        }
+
+        // Close pipe ends and update in_fd
         if (i < cmd_count - 1) {
             close(fd[1]);
             in_fd = fd[0];
         }
 
-        // Close input/output redirection file descriptors in parent process
         if (input_redirect != -1) {
             close(input_redirect);
         }
@@ -131,23 +142,23 @@ int process_command(char* cmdline){
         }
 
         // Free memory for each command
-        for(int j = 0; j < MAXARGS + 1; j++) {
+        for (int j = 0; j < MAXARGS + 1; j++) {
             free(arglist[j]);
         }
         free(arglist);
     }
 
-    // Wait for all children to finish
-    for (int i = 0; i < cmd_count; i++) {
-        wait(0);
+    if (is_background) {
+        printf("[Background] Process started with PID %d\n", getpid());
     }
 
     return 0;
 }
-char** tokenize(char* cmdline){
+
+char** tokenize(char* cmdline) {
     char** arglist = (char**)malloc(sizeof(char*) * (MAXARGS + 1));
     int argnum = 0;
-    
+
     char* token = strtok(cmdline, " \t\n");
     while (token != NULL && argnum < MAXARGS) {
         arglist[argnum] = (char*)malloc(strlen(token) + 1);
@@ -160,12 +171,12 @@ char** tokenize(char* cmdline){
     return arglist;
 }
 
-char* read_cmd(char* prompt, FILE* fp){
+char* read_cmd(char* prompt, FILE* fp) {
     printf("%s", prompt);
     int c;
     int pos = 0;
     char* cmdline = (char*)malloc(sizeof(char) * MAX_LEN);
-    while((c = getc(fp)) != EOF){
+    while((c = getc(fp)) != EOF) {
         if(c == '\n')
             break;
         cmdline[pos++] = c;
